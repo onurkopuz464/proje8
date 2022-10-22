d028fc3d6ade128dce9e9082c3b97400fb5929dc
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-22 17:58:29 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        public static void staircase(string[] people)
+    {
+        foreach (var item in people)
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+
+        static void Main(string[] args)
+        {
+           string[] peopleSend = {"ali","veli","hüsnü"};
+           staircase(peopleSend);
+         
+        
+
+        }
+        
+        
+      
+    }
+    
+}
